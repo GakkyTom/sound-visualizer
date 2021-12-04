d58e04e0d4f1e159d9ef95197a3e8e4eebcff981
GakkyTom <melooh42@gmail.com>
2021-12-04 18:27:13 +0900

    show mtkview with blue screen

--- SoundVisualizer/ViewController.swift
+++ SoundVisualizer/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import MetalKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var metalCircleView: MTKView!
+    
+    private let metalDevice = MTLCreateSystemDefaultDevice()!
+    
+    private var metalCommandQueue: MTLCommandQueue!
+    private var renderPipeline: MTLRenderPipelineState!
+    private let renderPassDescriptor = MTLRenderPassDescriptor()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupMetal()
+        metalCircleView.enableSetNeedsDisplay = true
+        metalCircleView.setNeedsDisplay()
     }
-
-
+    
+    private func setupMetal() {
+        metalCommandQueue = metalDevice.makeCommandQueue()
+        metalCircleView.device = metalDevice
+        metalCircleView.delegate = self
+    }
 }
 
+extension ViewController: MTKViewDelegate {
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        print("\(self.classForCoder)/" + #function)
+    }
+    
+    func draw(in view: MTKView) {
+        // Creating the commandBuffer for the queue
+        guard let commandBuffer = metalCommandQueue.makeCommandBuffer(),
+              // Creating the intervace for the pipeline
+              let renderDescriptor = view.currentRenderPassDescriptor
+        else { return }
+        
+        // Setting a background color
+        renderDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(1, 1, 0.5, 1)
+        
+        guard let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderDescriptor)
+        else { return }
+        
+        renderEncoder.endEncoding()
+        
+        commandBuffer.present(view.currentDrawable!)
+        
+        commandBuffer.commit()
+    }
+}
